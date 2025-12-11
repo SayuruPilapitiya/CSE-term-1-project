19533c43964dfd68fe2b8381f5764cc6ab46d22f
DeshanAyesh <deshanja17@gmail.com>
2025-12-11 19:21:10 +0530

    Im gong to make things work. This is the 0 step

--- al_book_link/lib/supabase.ts
+++ al_book_link/lib/supabase.ts
@@ -2,6 +2,11 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  "https://oocokhefavzkncowluey.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vY29raGVmYXZ6a25jb3dsdWV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMDM3ODgsImV4cCI6MjA3ODY3OTc4OH0.KKn_pk08v9Nv5URCeiKgACzSpX1d5OfikYt1tU1hFxU"
+
+
 );
+
+
+//process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
